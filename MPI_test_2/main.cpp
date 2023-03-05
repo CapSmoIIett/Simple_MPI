3ff2a8f88b072662b7b00c7c61c731e15d0c0c2b
unknown <vikdrovosek@gmail.com>
2023-03-05 05:18:46 +0300

    Work staff.

--- MPI_test_2/main.cpp
+++ MPI_test_2/main.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-#define SIZE_ROW 3000 
-#define SIZE_COL 3000 
+#define N 26
 
 int main()
 {
@@ -28,28 +27,71 @@ int main()
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
 
-    if (world_rank == 0)
-    {
-        int* a = new int [SIZE_ROW * SIZE_COL];
-        int* b = new int [SIZE_ROW * SIZE_COL];
-
-        for (int i = 0; i < SIZE_ROW; i++)
-            MPI_Send(a, SIZE_ROW, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD);
-        
-    }
-    else
-    {
-        int* a = new int [SIZE_COL];
-        int* b = new int [SIZE_COL];
-
-        MPI_Status status;
-
-        MPI_Recv(a, SIZE_ROW, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    }
 
     // Print off a hello world message
     printf("Hello world from processor %s, rank %d out of %d processors\n",
         processor_name, world_rank, world_size);
+
+    /*
+     * N > size
+     * N = size
+     * N < size
+     */
+
+    if (world_rank == 0)
+    {
+        int* a = new int [N];
+        int* b = new int [N * N];
+
+        for (int i = 0; i < N; i++)
+            a[i] = i;
+
+        int rank = 1;
+        for (int i = 0; i < N; i++)
+        {
+            if (rank <= world_size)
+                rank = 1;
+            int b = 1;
+            MPI_Send(&b, 1, MPI_INT, rank, rank, MPI_COMM_WORLD);
+            MPI_Send(a, N, MPI_INT, rank, rank, MPI_COMM_WORLD);
+            rank++;
+        }
+
+            for (int i = 0; i < N; i++)
+                printf("%d", a[i]);
+
+            rank = 1;
+        for (int i = 1; i < world_size; i++)
+        {
+            int b = 0;
+            MPI_Send(&b, 1, MPI_INT, rank, rank, MPI_COMM_WORLD);
+            rank++;
+        }
+        
+    }
+    else
+    {
+        int* a = new int [N];
+        int* b = new int [N];
+
+        MPI_Status status;
+
+        //MPI_Recv(a, N, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAGN MPI_COMM_WORLD, &status);
+        int s = 0;
+        while (true)
+        {
+            MPI_Recv(&s, 1, MPI_INT, 0, world_rank, MPI_COMM_WORLD, &status);
+            if (!s)
+                break;
+            MPI_Recv(a, N, MPI_INT, 0, world_rank, MPI_COMM_WORLD, &status);
+
+            printf("%s: ", processor_name);
+            for (int i = 0; i < N; i++)
+                printf("%d", a[i]);
+            printf("\n");
+        }
+
+    }
 
     // Finalize the MPI environment.
     MPI_Finalize();
